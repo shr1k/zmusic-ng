865e6f6ce0e0ecaa26d2190b0830795027c8543b
Shrikant N <shrikant@theswamp.in>
2013-06-03 15:57:47 +0100

    Local config changes.

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -35,12 +35,7 @@ $(function() {
 					loadSong();
 			});
 		}
-		var defaultChoices = [ "Wayne Shorter", "Bill Frisell", "Jimi Hendrix",
-				       "Allan Holdsworth", "Miles Davis", "Bill Evans",
-				       "Keith Jarret", "Kurt Rosenwinkel", "Gould", "Segovia",
-				       "Mike Stern", "John Coltrane", "McCoy Tyner",
-				       "BADBADNOTGOOD", "Charles Mingus", "Tribe Called Quest",
-				       "Lord of the Rings", "Pat Metheny", "Fugazi", "Blockhead" ];
+		var defaultChoices = ["Dido", "Eminem", "Spaccanapoli"];
 		$("#query").val(qParam("query") || defaultChoices[Math.floor(Math.random() * defaultChoices.length)]).change();
 	};
 
